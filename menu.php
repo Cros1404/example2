903853ddf8144cfd141226781dd19f33b0471aaf
Unknown <cros1404@gmail.com>
2017-03-16 12:00:37 +0200

    example2 things

---
+++ menu.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PHP example2</title>
+        <link rel="stylesheet" type="" href="mystyle.css">
+    </head>
+    <body>
+        <div id="menu">
+            <ul>
+        <li><a href="index.php">Mainpage</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="forms.php">Forms</a></li>
+            </ul>
+            
+        </div>
+        <div id="content">

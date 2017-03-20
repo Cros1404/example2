f86445ad6b91ebb6eacf1e74e088d13b192a1f87
Unknown <cros1404@gmail.com>
2017-03-20 09:45:54 +0200

    back end update

--- update_customer.php
+++ update_customer.php
@@ -2,6 +2,26 @@
 <h2>Edit customer</h2>
 
 <?php
-echo 'the id is '.$_GET['id']; ?>
+include "connection.php";
+$sql="SELECT * FROM customers WHERE id_customers=".$_GET['id'];
+$result=$db->query($sql);
+$theArray = $result->fetch(PDO::FETCH_ASSOC);
+$fn=$theArray['firstname'];
+$ln=$theArray['lastname'];
+$st=$theArray['streetaddress'];
+?>
+<form class="" action="customers.php" method="post">
+<label for="fn">Firstname</label><br />
+<input type="text" name="fn"id="fn" value="<?php echo $fn; ?>" required=""><br>
 
+<label for="ln">Lastname</label><br />
+<input type="text" name="ln" id="ln" value="<?php echo $ln; ?>" required=""><br>
+
+<label for="ad">Streetaddress</label><br />
+<input type="text" name="ad" id="ad" value="<?php echo $st; ?>" ><br>
+
+<input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
+<br>
+<input type="submit" name="btnEdit" value="Update">
+</form>
 <?php include "footer.php"; ?>

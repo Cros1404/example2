c4fe17899c29f575b045e713f62000573beecf7c
Unknown <cros1404@gmail.com>
2017-03-24 09:23:15 +0200

    show value not text

--- myFunctions.js
+++ myFunctions.js
@@ -1,5 +1,5 @@
 function showCountry(){
 	var source = document.getElementById("country") ;
-	var theText = source.options[source.selectedIndex].text;
+	var theText = source.options[source.selectedIndex].value;
 	result.value = theText;
 }
